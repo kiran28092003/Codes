90e3b567e8a897064ac14f0165f7e9bf9e36de9e
kiran28092003 <129212680+kiran28092003@users.noreply.github.com>
2024-09-22 00:28:34 +0530

    Add files via upload

---
+++ Constructor/Sconstructor2.java
@@ -0,0 +1,15 @@
+class String 
+{
+    public static void main(String []args)
+    {
+        public boolean isEmpty(str);
+        public boolean startsWith(String str);
+        public boolean endsWith(String str);
+        public trim String trim();
+        String sn=new String("maha       rashtra");
+        System.out.println(sn.trim);
+        public String subStrim(int index);
+        public String substrim(int startIndex,int end index);
+        
+    }
+}
