38d3357ddb2087e0f4d6b013490ce0bd6f44bfed
kiran28092003 <129212680+kiran28092003@users.noreply.github.com>
2024-09-23 21:09:25 +0530

    Update 05_constructor_demo_1.cpp

--- C++_Codes(1)/05_constructor_demo_1.cpp
+++ C++_Codes(1)/05_constructor_demo_1.cpp
@@ -26,5 +26,31 @@ class Date2{
         int month;
         int year;
     public:
-        Date2():
+        Date2(): day(1),month(1),year(1970){   
+        }
+        void show(){
+            pintf("%d/%d/%d\n",this->day,this->month,this->year);
+        }
+};
+void test1();
+void test2();
+int main(void){
+    test1();
+    test2();
+    return(0);
+}
+void test1(void)
+{
+    Date1 myDate1;
+
+    puts(""test1():entered");
+    myDate1.show();
+    puts("test1():leaving");
+}
+void test2(void)
+{
+    Date2 myDate2;
+    puts("test1():entered");
+    myDate2.show();
+    puts("test2():leaving");
 }
